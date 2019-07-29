916dc8da0fd447933893280482ac0f5bebb5e2b6
Will Hodgson <will.hodgson@current-rms.com>
2019-07-29 10:48:43 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -4,7 +4,9 @@ def my_all?(collection)
   block_return_values = []
   i = 0
   while i < collection.length
-    yield(collection[i])
+    if yield(collection[i])
+      block_return_values << collection[i]
+    end
     i += 1
   end
 end
